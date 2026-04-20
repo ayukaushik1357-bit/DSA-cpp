da8b28fb6c05d196001d8ffa4736e5ee53b814b3
ayukaushik1357-bit <ayukaushik1357@gmail.com>
2026-04-20 23:59:50 +0530

    Code

--- BINARY TREE/Binary_search_tree.cpp
+++ BINARY TREE/Binary_search_tree.cpp
@@ -21,7 +21,7 @@ Node* insertIntoBst(Node* &root,int d){
        return root;
     }
 
-    if(d<root->data){
+    if(d < root->data){
         insertIntoBst(root->left,d);
     }
     else{
@@ -76,6 +76,97 @@ void levelOrderTraversal(Node *root)
     }
 }
 
+
+void inorder(Node* root){
+    //Base case
+    if(root == NULL){
+        return;
+    }
+
+    inorder(root->left);
+    cout<<root->data<<" ";
+    inorder(root->right);
+}
+
+void preorder(Node* root){
+    //Base case
+    if(root == NULL){
+        return;
+    }
+
+    cout<<root->data<<" ";
+    preorder(root->left);
+    preorder(root->right);
+}
+
+void postorder(Node* root){
+    //Base case
+    if(root == NULL){
+        return;
+    }
+
+    postorder(root->left);
+    postorder(root->right);
+    cout<<root->data<<" ";
+}
+
+
+Node* minVal(Node* root){
+    Node* temp = root;
+    while(temp->left != NULL){
+        temp = temp->left;
+    }
+    return temp;
+}
+Node* maxVal(Node* root){
+    Node* temp = root;
+    while(temp->right != NULL){
+        temp = temp->right;
+    }
+    return temp;
+}
+
+Node* deleteNode(Node* root,int val){
+    if(root == NULL) 
+       return NULL;
+
+    if(root->data == val){
+        // 0 Child
+        if(root->left == NULL && root->right == NULL){
+            delete root;
+            return NULL;
+        }
+
+        // 1 Child
+        if(root->left != NULL && root->right == NULL){
+            Node* temp = root->left;
+            delete root;
+            return temp;
+        }
+        if(root->left == NULL && root->right != NULL){
+            Node* temp = root->right;
+            delete root;
+            return temp;
+        }
+
+        // 2 Child
+        if(root->left != NULL && root->right != NULL){
+            int mini = minVal(root->right)->data;
+            root->data = mini;
+            deleteNode(root->right,mini);
+            return root;
+        }
+    }
+    else if(root->data > val){
+        root->left = deleteNode(root->left,val);
+        return root;
+    }
+    else{
+        root->right = deleteNode(root->right,val);
+        return root;
+    }
+}
+
 int main()
 {
     Node* root = NULL;
@@ -85,6 +176,25 @@ int main()
     cout<<"Printing the BST"<<endl;
     levelOrderTraversal(root);
 
+    cout<<"Inorder traversal is: ";
+    inorder(root);
+    cout<<endl;
 
+    cout<<"Preorder traversal is: ";
+    preorder(root);
+    cout<<endl;
+
+    cout<<"Postorder traversal is: ";
+    postorder(root);
+    cout<<endl;
+
+    cout<<"Min value is "<<minVal(root)->data<<endl;
+    cout<<"Max value is "<<maxVal(root)->data<<endl;
+    
+    deleteNode(root,30);
+    cout<<"After delete node"<<endl;
+    levelOrderTraversal(root);
+
+    
     return 0;
 }
